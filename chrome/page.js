8aab17a305716f13bde6bd796f7dc29f9ceda79b
myf ma <myf@worldtalks.org>
2015-03-10 21:48:58 -0400

    using promise to see when webMM is ready in page, also added patch_function for both window.ready and chat.messageAdded

--- chrome/page.js
+++ chrome/page.js
@@ -1,15 +1,31 @@
 (function() {
-
-	var chat = WebMM.getCtrlInstants('chat');
-	var oldMessageAdded = chat.messageAdded;
-
-	chat.messageAdded = function(message){
-        if (message) {
-            console.log(message);
-            document.dispatchEvent(new CustomEvent('wesecure_message', {
-                detail: message
-            }));
-        }
-		oldMessageAdded.apply(this, arguments);
-	};
+    function patch_function(orig, patch) {
+        return function() {
+            patch.apply(this, arguments);
+            return orig.apply(this, arguments);
+        };
+    };
+    (new Promise(function(res, rej){
+        var chat = WebMM.getCtrlInstants('chat');
+        if (chat) {
+            res(chat);
+            return;
+        };
+        // check if chat is not ready, then we patch window.ready to get
+        // WebMM obj
+        window.ready = patch_function(window.ready, function(component) {
+            if (component === 'view') {
+                res(chat);
+            }
+        });
+    })).then(function(chat) {
+        chat.messageAdded = patch_function(chat.messageAdded, function(message){
+            if (message) {
+                console.log(message);
+                document.dispatchEvent(new CustomEvent('wesecure_message', {
+                    detail: message
+                }));
+            }
+        });
+    });
 })();
